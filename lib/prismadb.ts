dce4277b605506bea7aad7f1ae9b30a82e60f4d7
Ronald <ronald.dearmass@gmail.com>
2024-11-03 10:40:32 -0500

    conversationId fixed 3

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -6,9 +6,9 @@ declare global {
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 export const prisma = globalForPrisma.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
-}
+// if (process.env.NODE_ENV !== "production") {
+//   globalForPrisma.prisma = prisma;
+// }
 
 // import { PrismaClient } from '@prisma/client';
 
